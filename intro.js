2dabceff17a7d98f86c9eff61a94ee9e946234f0
Christian Martinez <christianandremartinez@gmail.com>
2023-08-09 20:46:42 -0600

    java1

---
+++ intro.js
@@ -0,0 +1,21 @@
+let variable = 8
+   
+let string = "Felipe"
+let x = "3"
+let boolean = 3+5
+let igual = string
+let array = [variable, boolean, igual, "ejemplo", 22]
+
+console.log(variable)
+console.log(string)
+console.log(x)
+console.log(boolean)
+console.log(igual)
+
+// Aqui vamos a ver algunas funciones de un array //
+console.log("Ahora vamos a ver los Array" )
+console.log(array) // mostrar el array completo //
+console.log(array[3]) // Para seleccionar un indice //
+console.log(array.length) // Para medir la longitud de un array //
+// La longitud de un array siempre va a ser la cantidad de indices que tenga mas 1, ya que comienza con el 0 //
+console.log(array[4])
