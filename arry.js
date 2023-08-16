856a84b4c194b2dbbab5e0bf499e18977bb276f6
Christian Martinez <christianandremartinez@gmail.com>
2023-08-15 20:49:52 -0600

    arrays

---
+++ arry.js
@@ -0,0 +1,9 @@
+let myString = 'Javacript+es+super+cool'
+let myArray = myString.split('+')
+    console.log(myArray)
+
+let arrayLenght = myArray.length;
+    console.log(arrayLenght)
+
+let lastitem = myArray[arrayLenght - 1]
+    console.log(lastitem);
