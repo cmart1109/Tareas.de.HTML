2fe10273d4de536e8bd7dfdc857afc9e70ffb19d
Christian Martinez <christianandremartinez@gmail.com>
2023-08-15 22:57:04 -0600

    Javaesmacizo

---
+++ Beer.js
@@ -0,0 +1,12 @@
+    console.log('COMPRA DE CERVEZA SUMAMENTE ILEGAL');
+let nombre = 'Christian'
+console.log('Hola ' + nombre + ', Gracias por ingresar a nuestra página');
+let edad = 19
+
+edad = parseInt(edad);
+
+if (edad >= 21) {
+    console.log('ACCESO CONCEDIDO, GRACIAS POR COMPRAR CERVEZA ILEGAL CON NOSOTROS, TENGA 🍺🍺🍺');
+} else {
+    console.log('ACCESO DENEGADO, NO CUMPLE CON NUESTROS REQUISITOS');
+}
--- Refran.js
+++ Refran.js
@@ -1,10 +1,16 @@
-let miRefran;
-miRefran = "Mas sabe el diablo por viejo, que por diablo";
+let miRefran = "Mas sabe el diablo por viejo, que por diablo";
 let longitudRefran = miRefran.length;
-let novenaLetra;
-novenaLetra = miRefran.charAt(8); // Obtén el carácter en la novena posición (índice 8)
-let ultimaLetra;
-ultimaLetra = miRefran.charAt(longitudRefran - 1); // Obtén el último carácter
+let novenaLetra = miRefran.charAt(8);
+let ultimaLetra = miRefran.charAt(longitudRefran - 1);
+let indicePalabra = miRefran.indexOf("que");
 
-// Que desgracia este 
+while (indicePalabra < 0) {
+    indicePalabra = miRefran.indexOf("a", indicePalabra + 1);
+}
 
+miRefran = miRefran.replace(miRefran.substring(indicePalabra, indicePalabra + 1), "xxx");
+
+let refranMayusculas = miRefran.toUpperCase();
+
+console.log("Refrán original:", miRefran);
+console.log("Refrán con preposición reemplazada:", refranMayusculas);
